8f4b39e70d0db9a48755e28a1708f4078226528f
Akihiro Nitta <nitta@akihironitta.com>
2020-10-28 21:57:19 +0900

    Update tests

--- tests/.download_datasets.py
+++ tests/.download_datasets.py
@@ -6,9 +6,7 @@ def main():
     try:
         ds = torchvision.datasets.MNIST(".cache/datasets", download=False)
     except:
-        print("dataset not found. downloading...")
         ds = torchvision.datasets.MNIST(".cache/datasets", download=True)
-        print("dataset downloaded.")
 
 
 if __name__ == '__main__':

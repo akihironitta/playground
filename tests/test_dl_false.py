bbc3d7cd4c4fffaedca182434f77e40bb8752970
Akihiro Nitta <nitta@akihironitta.com>
2020-10-29 23:33:19 +0900

    Add new test

--- tests/test_dl_false.py
+++ tests/test_dl_false.py
@@ -7,3 +7,7 @@ from torchvision.datasets import MNIST
 def test_dl_false(datadir):
     warn(f"datadir='{datadir}'")
     dataset = MNIST(datadir, download=False)
+
+def test_dl_false2(datadir):
+    warn(f"datadir='{datadir}'")
+    dataset = MNIST(datadir, download=False)

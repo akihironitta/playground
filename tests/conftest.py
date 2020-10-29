430153b92190d02ff80263da23fb95186cbfd4a8
Akihiro Nitta <nitta@akihironitta.com>
2020-10-29 23:50:45 +0900

    keep datadir

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import pytest
 
 
 @pytest.fixture(scope="session")
+def datadir(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("datasets")
+    return fn
+
+@pytest.fixture(scope="session")
 def tmpdir(tmpdir_factory):
     fn = tmpdir_factory.mktemp("datasets")
     return fn

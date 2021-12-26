fad609eb0a595437537d2f4ebb9e18b7ac9adb0a
Akihiro Nitta <nitta@akihironitta.com>
2021-12-26 12:55:40 +0900

    Add a script that always causes segfault

--- tests/test_dl_false.py
+++ tests/test_dl_false.py
@@ -1,13 +1,3 @@
-from warnings import warn
-
-import torch
-from torchvision.datasets import MNIST
-
-
-def test_dl_false(datadir, tmpdir):
-    warn(f"datadir='{datadir}' tmpdir='{tmpdir}'")
-    dataset = MNIST(datadir, download=False)
-
-def test_dl_false2(datadir, tmpdir):
-    warn(f"datadir='{datadir}' tmpdir='{tmpdir}'")
-    dataset = MNIST(datadir, download=False)
+def test_segfault(datadir, tmpdir):
+    import ctypes
+    ctypes.string_at(0)  # segfault on purpose

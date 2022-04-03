7847e3f36eae0a185f5d3ad61a10d3597fd28f52
Akihiro Nitta <nitta@akihironitta.com>
2022-04-03 17:55:14 +0900

    Update test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,5 +1,10 @@
+import warnings
+
 from src import print_os
 
 
 def test_print_os(datadir, tmpdir):
-    assert isinstance(print_os(), str)
+    warnings.warn("some message", DeprecationWarning)
+    warnings.warn(f"datadir: {datadir}", FutureWarning)
+    warnings.warn(f"tmpdir: {tmpdir}", UserWarning)
+    assert print_os() is None

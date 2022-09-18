8eb495f03bb9c1433aa0c7c4d64d2606743da68e
Akihiro Nitta <nitta@akihironitta.com>
2022-09-18 15:05:36 +0900

    Update test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,10 +1,36 @@
-import warnings
+import pytest
+import sys
 
-from src import print_os
+
+def test_ok():
+    assert True
 
 
-def test_print_os(datadir, tmpdir):
-    warnings.warn("some message", DeprecationWarning)
-    warnings.warn(f"datadir: {datadir}", FutureWarning)
-    warnings.warn(f"tmpdir: {tmpdir}", UserWarning)
-    assert print_os() is None
+def test_fail():
+    assert False
+
+
+@pytest.mark.xfail    
+def test_xfail():
+    assert False
+
+    
+@pytest.mark.skipif(True, reason="always gets skipped")
+def test_skip():
+    assert False
+
+    
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python 3.10 or higher")
+def test_skip_if_python_le_3_10():
+    assert True
+
+    
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python 3.9 or higher")
+def test_skip_if_python_le_3_9():
+    assert True
+
+    
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8 or higher")
+def test_skip_if_python_le_3_8():
+    assert True
+
